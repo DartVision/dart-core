3d612b737eb591da8e60d7044010535a7e6030cd
Nikolas Klug <klug.nikolas@googlemail.com>
2020-04-18 16:36:34 +0200

    Continuously take pictures and provide latest

--- python/camera-server/api.py
+++ python/camera-server/api.py
@@ -4,11 +4,13 @@ from camera import Camera
 app = Bottle()
 camera = Camera()
 
+
 @app.route('/image.jpg')
-def hello():
+def get_image():
     image = camera.capture()
     response.set_header('Content-type', 'image/jpeg')
     return image
 
 
+# Start the server on the external interface
 run(app, host='0.0.0.0', port=80)
--- python/camera-server/camera.py
+++ python/camera-server/camera.py
@@ -1,6 +1,9 @@
 from time import sleep
 from io import BytesIO
+from threading import Thread, Lock
 from picamera import PiCamera
+import copy
+import logging
 
 
 class Camera(object):
@@ -8,21 +11,24 @@ class Camera(object):
         self._camera = PiCamera()
         self._camera.resolution = (3280, 2464)
         self._camera.start_preview()
+        self._image = None
+        self._lock = Lock()
+        self._thread = Thread(target=self._start_capture)
         # Camera warm-up time
         sleep(2)
+        self._thread.start()
 
     def capture(self):
-        buffer = BytesIO()
-        self._camera.capture(buffer, format='jpeg', quality=100)
-        buffer.seek(0)
-        return buffer.read()
+        self._lock.acquire()
+        image = copy.copy(self._image)
+        self._lock.release()
+        return image
 
-
-if __name__ == '__main__':
-    import time
-
-    camera = Camera()
-    t0 = time.time_ns()
-    x = camera.capture()
-    t1 = time.time_ns()
-    print((t1 - t0) / 1000000)
+    def _start_capture(self):
+        while True:
+            buffer = BytesIO()
+            self._camera.capture(buffer, format='jpeg', quality=100)
+            buffer.seek(0)
+            self._lock.acquire()
+            self._image = buffer.read()
+            self._lock.release()
