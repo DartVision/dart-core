b19b84b67b12b2c4b7fe9a5f811c51fd70d66edc
Nikolas Klug <klug.nikolas@googlemail.com>
2021-03-18 13:28:17 +0100

    Use flask instead of bottle for camera server

--- python/camera-server/start.py
+++ python/camera-server/start.py
@@ -1,16 +1,17 @@
-from bottle import Bottle, run, response
+import flask
 from camera import Camera
 
-app = Bottle()
+app = flask.Flask(__name__)
 camera = Camera()
-
 
 @app.route('/image.jpg')
 def get_image():
+    response = flask.Response()
     image = camera.capture()
-    response.set_header('Content-type', 'image/jpeg')
-    return image
+    response.headers['Content-type'] = 'image/jpeg'
+    headers = {'Content-type': 'image/jpeg'}
+    return image, headers
 
 
 # Start the server on the external interface
-run(app, host='0.0.0.0', port=80)
+app.run(host='0.0.0.0', port=8082, threaded=True)
